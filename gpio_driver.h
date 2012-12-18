8a40d33d1c18548a8910f28dc98e8ff7f40c817f
Alexander Weld/pi <weldale@gmail.com>
2012-12-18 21:38:00 +0000

    gpio driver

---
+++ gpio_driver.h
@@ -0,0 +1,26 @@
+// GPIO driver
+// Adapted from http://elinux.org/RPi_Low-level_peripherals by <weldale@gmail.com>
+// 2012
+
+#ifndef __GPIO_DRIVER_H
+#define __GPIO_DRIVER_H
+
+#define BCM2708_PERI_BASE        0x20000000
+#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+
+#define PAGE_SIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+void gpio_setup();
+
+extern volatile unsigned *gpio;
+
+#endif
+
